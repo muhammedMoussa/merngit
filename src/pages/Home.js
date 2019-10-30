7a5920b9e362d7f2fb20e3f1fc2a631014102b0c
Muhammed Moussa <muhammedmoussa@hotmail.com>
2019-10-30 23:42:04 +0200

    added routing

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+
+      <div class="ui grid stackable">
+        <div class="six wide column">
+            <div class="four wide column"></div>
+            <div class="four wide column"></div>
+            <div class="four wide column"></div>
+            <div class="four wide column"></div>
+        </div>
+        <div class="six wide column"></div>
+    </div>
+
+    </div>
+  );
+}
+
+export default Home;
